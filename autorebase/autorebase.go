f353ad551198915eae56c228a14acfe51b5b2d86
Jimmy Song <jaejoon@gmail.com>
2014-06-30 09:32:19 -0500

    Added automatic adding of upstream repository to the monetas repos.
    Does not work otherwise.

--- autorebase/autorebase.go
+++ autorebase/autorebase.go
@@ -25,9 +25,14 @@ func main() {
 		repo := filepath.Join(monetas, file.Name())
 		fmt.Println("Processing", repo)
 		os.Chdir(repo)
+		// check that upstream is actually defined
 		exec.Command("git", "checkout", "master").Output()
 		exec.Command("git", "pull").Output()
+		up := fmt.Sprintf("git@github.com:/conformal/%v.git", file.Name())
+		fmt.Println("upstream", up)
+		exec.Command("git", "remote", "add", "upstream", up).Output()
 		exec.Command("git", "fetch", "upstream").Output()
+
 		out, _ := exec.Command("git", "rebase", "upstream/master").Output()
 		s := string(out)
 		if strings.Contains(s, "CONFLICT") {
