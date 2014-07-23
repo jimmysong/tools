2a51b90559c975eee2d231f90bc7e7804c9bcd49
Nicola 'tekNico' Larosa <nico@teknico.net>
2014-07-23 17:32:46 +0200

    Add a 'stopall' option, rename the 'closewallet' option and related code to 'stopwallets', refactor the whole thing.

--- multiwallet/multiwallet.go
+++ multiwallet/multiwallet.go
@@ -17,82 +17,44 @@ import (
 	"github.com/monetas/btcutil"
 )
 
-func main() {
-	// Parse which network we're using.
-	var simnet bool
-	var mainnet bool
-	var closewallet bool
-	flag.BoolVar(&simnet, "simnet", false, "connect to simnet")
-	flag.BoolVar(&mainnet, "mainnet", false, "connect to mainnet")
-	flag.BoolVar(&closewallet, "closewallet", false, "close wallet processes")
-	flag.Parse()
-
-	// Get the root cert for connecting to secure websocket.
-	btcwalletHomeDir := btcutil.AppDataDir("btcwallet", false)
-
-	if closewallet {
-		// Close all the btcwallet processes.
-		files, _ := ioutil.ReadDir(btcwalletHomeDir)
-		for _, file := range files {
-			port, err := strconv.Atoi(file.Name())
-			if err != nil || port < 8400 || port > 28409 {
-				continue
-			}
-			// Get contents of pid file.
-			pid, err := ioutil.ReadFile(filepath.Join(btcwalletHomeDir, file.Name(), "pid"))
-			if err != nil {
-				fmt.Printf("closewallet get %v pid: %v\n", port, err)
-				continue
-			}
-			_, err = exec.Command("kill", string(pid)).Output()
-			if err != nil {
-				fmt.Printf("closewallet kill %v: %v\n", port, err)
-			}
-		}
-		return
-	}
-
-	// The starting port used by the btcwallet processes are determined as follows:
-	//  mainnet: 8400
-	//  testnet: 18400 (default)
-	//  simnet : 28400
-	var startport int
-	if mainnet {
-		startport = 8400
-	} else if simnet {
-		startport = 28400
-	} else {
-		startport = 18400
-	}
-
-	// TODO: check if btcd and btcwallet are installed.
-
-	// Start up btcd.
-	path := os.Getenv("GOPATH")
-	btcd := filepath.Join(path, "bin", "btcd")
-	var cmd *exec.Cmd
-	if mainnet {
-		cmd = exec.Command(btcd)
-	} else if simnet {
-		cmd = exec.Command(btcd, "--simnet")
-	} else {
-		cmd = exec.Command(btcd, "--testnet")
-	}
-
+func startBtcd(cmd *exec.Cmd, homeDir string) {
 	err := cmd.Start()
 	if err != nil {
 		log.Fatalf("btcd start: %v", err)
 	}
+	// Record the process id.
+	file, err := os.Create(filepath.Join(homeDir, "pid"))
+	if err != nil {
+		log.Fatalf("btcd pid file creation: %v", err)
+	}
+	pid := fmt.Sprintf("%v", cmd.Process.Pid)
+	file.WriteString(pid)
+	file.Close()
+}
 
+func stopBtcd(homeDir string) {
+	// Get contents of pid file.
+	pid, err := ioutil.ReadFile(filepath.Join(homeDir, "pid"))
+	if err != nil {
+		fmt.Printf("stopBtcd get pid: %v\n", err)
+		return
+	}
+	_, err = exec.Command("kill", string(pid)).Output()
+	if err != nil {
+		fmt.Printf("stopBtcd kill: %v\n", err)
+	}
+}
+
+func startAllWallets(gopath string, startport int, btcwalletHomeDir string) {
 	// Start up 10 btcwallet processes.
-	btcwallet := filepath.Join(path, "bin", "btcwallet")
-	btcctl := filepath.Join(path, "bin", "btcctl")
+	btcwallet := filepath.Join(gopath, "bin", "btcwallet")
+	btcctl := filepath.Join(gopath, "bin", "btcctl")
 	for i := 0; i < 10; i++ {
 		port := startport + i
 		listen := fmt.Sprintf("--rpclisten=127.0.0.1:%v", port)
 		dir := filepath.Join(btcwalletHomeDir, fmt.Sprintf("%v", port))
-		// Directory creation or the pid file creation will fail.
-		err = os.Mkdir(dir, os.ModeDir|0700)
+		// Make the wallet directory beforehand or the pid file creation will fail.
+		err := os.Mkdir(dir, os.ModeDir|0700)
 		if e, ok := err.(*os.PathError); ok && e.Err != syscall.EEXIST {
 			log.Fatalf("home dir creation: %v", err)
 		}
@@ -110,7 +72,7 @@ func main() {
 		}
 
 		datadir := fmt.Sprintf("--datadir=%v", dir)
-		cmd = exec.Command(btcwallet, "--username=user", "--password=pass", listen, datadir)
+		cmd := exec.Command(btcwallet, "--username=user", "--password=pass", listen, datadir)
 		err = cmd.Start()
 		if err != nil {
 			log.Fatalf("%v btcwallet start: %v", port, err)
@@ -119,7 +81,7 @@ func main() {
 		// Record the process id.
 		file, err := os.Create(filepath.Join(dir, "pid"))
 		if err != nil {
-			log.Fatalf("pid file creation: %v", err)
+			log.Fatalf("btcwallet pid file creation: %v", err)
 		}
 		pid := fmt.Sprintf("%v", cmd.Process.Pid)
 		file.WriteString(pid)
@@ -132,4 +94,75 @@ func main() {
 			fmt.Printf("createencryptedwallet %v: %v\n", port, err)
 		}
 	}
+}
+
+func stopAllWallets(btcwalletHomeDir string) {
+	files, _ := ioutil.ReadDir(btcwalletHomeDir)
+	for _, file := range files {
+		port, err := strconv.Atoi(file.Name())
+		if err != nil || port < 8400 || port > 28409 {
+			continue
+		}
+		// Get contents of pid file.
+		pid, err := ioutil.ReadFile(filepath.Join(btcwalletHomeDir, file.Name(), "pid"))
+		if err != nil {
+			fmt.Printf("stopAllWallets get %v pid: %v\n", port, err)
+			continue
+		}
+		_, err = exec.Command("kill", string(pid)).Output()
+		if err != nil {
+			fmt.Printf("stopAllWallets kill %v: %v\n", port, err)
+		}
+	}
+}
+
+func main() {
+	var mainnet bool
+	flag.BoolVar(&mainnet, "mainnet", false, "connect to mainnet")
+	var simnet bool
+	flag.BoolVar(&simnet, "simnet", false, "connect to simnet")
+	var stopall bool
+	flag.BoolVar(&stopall, "stopall", false, "stop btcd and wallets")
+	var stopwallets bool
+	flag.BoolVar(&stopwallets, "stopwallets", false, "stop wallet processes")
+	flag.Parse()
+
+	gopath := os.Getenv("GOPATH")
+	btcdHomeDir := btcutil.AppDataDir("btcd", false)
+	btcwalletHomeDir := btcutil.AppDataDir("btcwallet", false)
+
+	if stopall {
+		stopBtcd(btcdHomeDir)
+		stopAllWallets(btcwalletHomeDir)
+		return
+	}
+
+	if stopwallets {
+		stopAllWallets(btcwalletHomeDir)
+		return
+	}
+
+	btcdPath := filepath.Join(gopath, "bin", "btcd")
+	var btcdCmd *exec.Cmd
+	if mainnet {
+		btcdCmd = exec.Command(btcdPath)
+	} else if simnet {
+		btcdCmd = exec.Command(btcdPath, "--simnet")
+	} else {
+		btcdCmd = exec.Command(btcdPath, "--testnet")
+	}
+	// TODO: check if btcd and btcwallet are installed.
+	startBtcd(btcdCmd, btcdHomeDir)
+
+	// The starting port used by the btcwallet processes is:
+	// mainnet: 8400 - testnet: 18400 (default) - simnet : 28400
+	var startport int
+	if mainnet {
+		startport = 8400
+	} else if simnet {
+		startport = 28400
+	} else {
+		startport = 18400
+	}
+	startAllWallets(gopath, startport, btcwalletHomeDir)
 }
